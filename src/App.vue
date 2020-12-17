846272f650e996a6e442e146025d8937d3cea3a3
Rahul Gandhi <gandhirahul@users.noreply.github.com>
2020-12-17 18:24:22 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="app">
+        <h1>Twitter with Vuejs</h1>
+        <TwitterFeed/>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import TwitterFeed from './components/TwitterFeed.vue'
+
+@Component({
+    components: { TwitterFeed },
+})
+export default class App extends Vue {
+}
+</script>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
